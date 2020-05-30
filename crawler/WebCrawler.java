35ba9a37b7efa03badd47f7a9478adfc48400047
mlad65 <24.alexander.kim@gmail.com>
2020-05-30 12:49:50 +1100

    Get rid of useless code. Still need polishing

--- crawler/WebCrawler.java
+++ crawler/WebCrawler.java
@@ -27,19 +27,11 @@ public class WebCrawler extends JFrame {
     private JLabel titleLabel;
     private JTable titlesTable;
     private JTextField exportUrlTextField;
-    private String[] titlesTableHeader = new String[] {"URL", "Title"};
-    private ConcurrentLinkedQueue<String[]> dataQueue = new ConcurrentLinkedQueue<>();
     private Map<String, String> mapData = new LinkedHashMap<>();
     private Set<String> visitedLinks = new ConcurrentSkipListSet<>();
     private ConcurrentLinkedQueue<String[]> urlQueue = new ConcurrentLinkedQueue<>();
-    private final Pattern patternTitle = Pattern.compile("(<title[\\w=\\-\"]*>)(.*?)(</title>)");
-    private final Pattern patternTag = Pattern.compile("(<a.*?href=[\"'])(.*?)([\"'].*?>)(.*?)(</a>)");
-    private final Pattern patternBaseUrl = Pattern.compile("(https?://)([\\w.-]+)(.*?)(/?)([^/]*)");
-    private final Pattern patternNormalUrl = Pattern.compile("https?://.*?");
-    private final Pattern patternRelativeUrl = Pattern.compile("(/?)([\\w.%/-]*)");
     private int depthLimit = 2;
     private int workerCount = 5;
-    private int parsedPagesCount = 0;
 
     public WebCrawler() {
         super("Web Crawler");
@@ -170,47 +162,12 @@ public class WebCrawler extends JFrame {
 
     private void exportToFile() {
         try (PrintWriter writer = new PrintWriter(exportUrlTextField.getText())) {
-            String[] entry;
-            /*
-            while (dataQueue.peek() != null) {
-                entry = dataQueue.poll();
-                writer.println(entry[0]);
-                writer.println(entry[1]);
-            }
-             */
             mapData.forEach((k, v) -> {
                 writer.println(k);
                 writer.println(v);
             });
         } catch (IOException ioException) {
             ioException.printStackTrace();
-        }
-    }
-
-    private void parseHtml() {
-        try {
-            final String url = urlTextField.getText();
-            final InputStream inputStream = new URL(url).openStream();
-            final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-            final StringBuilder stringBuilder = new StringBuilder();
-            final String LINE_SEPARATOR = System.getProperty("line.separator");
-
-            String nextLine;
-            while ((nextLine = reader.readLine()) != null) {
-                stringBuilder.append(nextLine);
-                stringBuilder.append(LINE_SEPARATOR);
-            }
-            final String siteText = stringBuilder.toString();
-            Matcher matcher = patternTitle.matcher(siteText);
-            if (matcher.find()) {
-                titleLabel.setText(matcher.group(2));
-            } else {
-                titleLabel.setText("No title");
-            }
-            htmlTextArea.setText(siteText);
-            mapData.put(url, titleLabel.getText());
-        } catch (Exception exception) {
-            htmlTextArea.setText(exception.getMessage());
         }
     }
 
@@ -222,7 +179,6 @@ public class WebCrawler extends JFrame {
             workerCount = 10;
             CrawlerThread[] threads = new CrawlerThread[workerCount];
             depthLimit = Integer.parseInt(depthTextField.getText());
-            //if (depthLimit > 100) depthLimit = 100;
             int depthLevel = 0;
             String[] url;
             int i = 0;
@@ -254,13 +210,6 @@ public class WebCrawler extends JFrame {
                 parsedLabel.setText(String.valueOf(mapData.size()));
             }
             parsedLabel.setText(String.valueOf(mapData.size()));
-            if (!urlQueue.isEmpty()) {
-                try (PrintWriter writer = new PrintWriter("C:\\Apps\\errorlog.txt")) {
-                    writer.print(urlQueue.poll().toString());
-                } catch (FileNotFoundException fileNotFoundException) {
-                    fileNotFoundException.printStackTrace();
-                }
-            }
         } catch (Exception e) {
             try (PrintWriter writer = new PrintWriter("C:\\Apps\\errorlog.txt")) {
                 writer.print(e.toString());
@@ -269,173 +218,6 @@ public class WebCrawler extends JFrame {
             }
         }
     }
-
-
-    private void getLinks() {
-        try {
-            mapData.clear();
-            parseHtml();
-            Matcher matcherTag = patternTag.matcher(htmlTextArea.getText());
-            Matcher matcherUrl;
-            String baseUrl;
-            String currentUrl;
-            String urlString;
-            Matcher matcherBaseUrl = patternBaseUrl.matcher(urlTextField.getText());
-            if (matcherBaseUrl.matches()) {
-                baseUrl = matcherBaseUrl.group(1) + matcherBaseUrl.group(2);
-                currentUrl = matcherBaseUrl.group(1) + matcherBaseUrl.group(2) + matcherBaseUrl.group(3) + matcherBaseUrl.group(4);
-            } else {
-                baseUrl = "http://localhost";
-                currentUrl = "http://localhost/";
-            }
-            while (matcherTag.find()) {
-                if (patternNormalUrl.matcher(matcherTag.group(2)).matches()) {
-                    addToMapData(matcherTag.group(2));
-                } else {
-                    matcherUrl = patternRelativeUrl.matcher(matcherTag.group(2));
-                    if (matcherUrl.matches()) {
-                        if (matcherUrl.group(1).length() == 1) {
-                            urlString = baseUrl + matcherUrl.group(0);
-                        } else {
-                            urlString = currentUrl + matcherUrl.group(0);
-                        }
-                        if (!addToMapData(urlString)) {
-                            if (matcherUrl.group(1).length() == 1) {
-                                urlString = "http:/" + matcherUrl.group(0);
-                            } else {
-                                urlString = "http://" + matcherUrl.group(0);
-                            }
-                            if (!addToMapData(urlString)) {
-                                if (matcherUrl.group(1).length() == 1) {
-                                    urlString = "https:/" + matcherUrl.group(0);
-                                } else {
-                                    urlString = "https://" + matcherUrl.group(0);
-                                }
-                                addToMapData(urlString);
-                            }
-                        }
-                    }
-                }
-            }
-            String[][] tableData = new String[mapData.size()][];
-            int i = 0;
-            for (Map.Entry<String, String> entry : mapData.entrySet()) {
-                tableData[i] = new String[] { entry.getKey(), entry.getValue() };
-                i++;
-            }
-            if (i > 0) {
-                titlesTable.setModel(new DefaultTableModel(tableData, titlesTableHeader));
-                titlesTable.setEnabled(true);
-            }
-        } catch (Exception exception) {
-             htmlTextArea.setText(exception.getMessage());
-        }
-    }
-
-    private boolean addToMapData(String url) {
-        try {
-            URL urlObject = new URL(url);
-            URLConnection connection = urlObject.openConnection();
-            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0");
-            if (connection.getContentType() != null) {
-                if (connection.getContentType().contains("text/html")) {
-                    mapData.put(url, findTitleInUrl(connection));
-                    return true;
-                }
-            }
-            return false;
-        } catch (IOException exception) {
-            htmlTextArea.setText(exception.getMessage());
-            return false;
-        }
-    }
-
-    private void crawlPage(String url) throws IOException {
-        String nextUrl;
-        String baseUrl;
-        String currentUrl;
-        Matcher matcherTag = patternTag.matcher(getHtml(url));
-        Matcher matcherBaseUrl = patternBaseUrl.matcher(url);
-        if (matcherBaseUrl.matches()) {
-            baseUrl = matcherBaseUrl.group(1) + matcherBaseUrl.group(2);
-            currentUrl = matcherBaseUrl.group(1) + matcherBaseUrl.group(2) + matcherBaseUrl.group(3) + matcherBaseUrl.group(4);
-        } else {
-            baseUrl = "http://localhost";
-            currentUrl = "http://localhost/";
-        }
-        while (matcherTag.find()) {
-            nextUrl = getValidUrl(matcherTag.group(2), baseUrl, currentUrl);
-            if (!"".equals(nextUrl)) {
-                crawlPage(url);
-            }
-        }
-    }
-
-    private String findTitleInUrl(URLConnection connection) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-        String nextLine;
-        StringBuilder stringBuilder = new StringBuilder();
-        while ((nextLine = reader.readLine()) != null) {
-            stringBuilder.append(nextLine);
-            stringBuilder.append(LINE_SEPARATOR);
-        }
-        Matcher matcherTitle = patternTitle.matcher(stringBuilder);
-        if (matcherTitle.find()) {
-            return matcherTitle.group(2);
-        }
-        return "No title";
-    }
-
-    private String getHtml(String url) throws IOException {
-        final InputStream inputStream = new URL(url).openStream();
-        final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-        final StringBuilder stringBuilder = new StringBuilder();
-        final String LINE_SEPARATOR = System.getProperty("line.separator");
-
-        String nextLine;
-        while ((nextLine = reader.readLine()) != null) {
-            stringBuilder.append(nextLine);
-            stringBuilder.append(LINE_SEPARATOR);
-        }
-        return stringBuilder.toString();
-    }
-
-    private String getValidUrl(String attrHref, String baseUrl, String currentUrl) throws IOException {
-        String url;
-        if (patternNormalUrl.matcher(attrHref).matches()) {
-            if (addToMapData(attrHref)) return attrHref;
-        } else {
-            Matcher matcherUrl = patternRelativeUrl.matcher(attrHref);
-            if (matcherUrl.matches()) {
-                if (matcherUrl.group(1).length() == 1) {
-                    url = baseUrl + matcherUrl.group(0);
-                } else {
-                    url = currentUrl + matcherUrl.group(0);
-                }
-                if (addToMapData(url)) {
-                    return url;
-                } else {
-                    if (matcherUrl.group(1).length() == 1) {
-                        url = "http:/" + matcherUrl.group(0);
-                    } else {
-                        url = "http://" + matcherUrl.group(0);
-                    }
-                    if (addToMapData(url)) {
-                        return url;
-                    } else {
-                        if (matcherUrl.group(1).length() == 1) {
-                            url = "https:/" + matcherUrl.group(0);
-                        } else {
-                            url = "https://" + matcherUrl.group(0);
-                        }
-                        if (addToMapData(url)) return url;
-                    }
-                }
-            }
-        }
-        return "";
-    }
-
 }
 
 class CrawlerThread extends Thread {
@@ -446,7 +228,6 @@ class CrawlerThread extends Thread {
     private final Pattern patternBaseUrl = Pattern.compile("(https?://)([\\w.:-]+)(.*?)(/?)([^/]*)");
     private final Pattern patternNormalUrl = Pattern.compile("https?://.*?");
     private final Pattern patternRelativeUrl = Pattern.compile("(/?)(.*)");
-    private ConcurrentLinkedQueue<String[]> dataQueue;
     private Map<String, String> mapData;
     private Set<String> visitedLinks;
     private String url;
@@ -518,7 +299,7 @@ class CrawlerThread extends Thread {
                 stringBuilder.append(LINE_SEPARATOR);
             }
             return stringBuilder.toString();
-        } catch (IOException e) {
+        } catch (Exception e) {
             try (PrintWriter writer = new PrintWriter("C:\\Apps\\errorlog.txt")) {
                 writer.print(e.toString());
             } catch (FileNotFoundException fileNotFoundException) {
@@ -545,7 +326,6 @@ class CrawlerThread extends Thread {
                     } else {
                         title = "No title";
                     }
-                    //dataQueue.offer(new String[] { url, title });
                     synchronized (CrawlerThread.class) {
                         mapData.put(url, title);
                         visitedLinks.add(url);
